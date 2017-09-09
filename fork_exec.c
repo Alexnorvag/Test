07629ad508ee932429c617d10bb4ffad350ddbc9
Alexnorvag <norvag@mail.ru>
2017-09-09 19:32:39 +0300

    good

--- fork_exec.c
+++ fork_exec.c
@@ -42,57 +42,62 @@ int main(int argc, char* argv[]) {
 			printf("CLIENT: 1\n");
 			ChildProcess();
 		}
-		if (exec_number == 2) {
-			printf("@@@\n");	
-		}
-		printf("before\n");
 		exit(0);
 	}
 	if (pid > 0) {
 		printf("SERVER: STARTED\n");
 		
-		if (exec_number == 1) {client_to_server = open(myfifo1, O_RDONLY);
-   		server_to_client = open(myfifo2, O_WRONLY);}
+		if (exec_number == 1) {
+			client_to_server = open(myfifo1, O_RDONLY);
+   			server_to_client = open(myfifo2, O_WRONLY);
+   		}
 
-   		printf("SS\n");
 		waitpid(pid, 0, 0);
 		
-		printf("AFTERSERVER\n");
-		
-		//read(client_to_server, buf, BUFSIZ);
+		if (exec_number == 1) {
+			printf("AFTERSERVER\n");
+			read(client_to_server, buf, BUFSIZ);
+		}
 
-		if (strcmp("",buf)!=0)
+		if (strcmp("", buf) != 0)
       		{
          		printf("Received: %s\n", buf);
          		//write(server_to_client,buf,BUFSIZ);
       		}
 
-		if (exec_number == 1) {close(client_to_server);
-   		close(server_to_client);}
+		if (exec_number == 1) {
+			close(client_to_server);
+   			close(server_to_client);
+		}
 		
 		pid = fork();
 		if (pid == 0) {
-			printf("YES\n");
 			if (exec_number == 2) {
 				printf("CLIENT: 2\n");
 				ChildProcess();
-			} else exit(1);
+			} else exit(0);
 		}
 		if (pid > 0) {
+			if (exec_number == 2) {
 			client_to_server = open(myfifo1, O_RDONLY);
    			server_to_client = open(myfifo2, O_WRONLY);
+   			}
 
 			waitpid(pid, 0, 0);
 
-			read(client_to_server, buf, BUFSIZ);
-			if (strcmp("",buf)!=0)
-      			{
-         			printf("Received: %s\n", buf);
-         			//write(server_to_client,buf,BUFSIZ);
-      			}			
+			if (exec_number == 2) {
+				read(client_to_server, buf, BUFSIZ);
+				if (strcmp("",buf)!=0)
+      				{
+         				printf("Received: %s\n", buf);
+         				//write(server_to_client,buf,BUFSIZ);
+      				}	
+      			}		
 			
+			if (exec_number == 2) {
 			close(client_to_server);
    			close(server_to_client);
+   			}
 		}
 	}
 
