00e947d9d84949e8d7f23023552e36c4eaed3fc8
Alexnorvag <norvag@mail.ru>
2017-09-10 15:24:24 +0300

    meassage queue

--- fork_exec.c
+++ fork_exec.c
@@ -5,15 +5,38 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct my_msgbuf {
+    long mtype;
+    char mtext[200];
+};
 
 void ChildProcess();
 
 int main(int argc, char* argv[]) {
+
 	int exec_number = atoi(argv[1]);
 	if (exec_number != 1 && exec_number != 2) {
 		printf("Invalid input. Try this: ./name.o [number_executable_client]");
 		return 0;
 	}
+	
+	struct my_msgbuf Buf;
+    	int msqid;
+    	key_t key;
+    	
+    	if ((key = ftok("fork_exec.c", 'B')) == -1) {
+        	perror("ftok");
+        	exit(1);
+    	}
+    	
+    	if ((msqid = msgget(key, 0644 | IPC_CREAT)) == -1) {
+        	perror("msgget");
+        	exit(1);
+    	}
 	
 	int client_to_server;
 	char *myfifo1 = "/tmp/client_to_server_fifo";
@@ -49,20 +72,37 @@ int main(int argc, char* argv[]) {
 			
 			client_to_server = open(myfifo1, O_RDONLY);
    			server_to_client = open(myfifo2, O_WRONLY);
+      			//msgque
+   			printf("Enter lines of text, ^D to quit:\n");
+			Buf.mtype = 1;
 	
+			/*while(*/fgets(Buf.mtext, sizeof Buf.mtext, stdin);// != NULL) {
+        		int len = strlen(Buf.mtext);
+
+        		if (Buf.mtext[len-1] == '\n') Buf.mtext[len-1] = '\0';
+
+        		if (msgsnd(msqid, &Buf, len+1, 0) == -1) /* +1 for '\0' */
+            			perror("msgsnd");
+    			//}
+
+   			if (msgctl(msqid, IPC_RMID, NULL) == -1) {
+        			perror("msgctl");
+        			exit(1);
+    			}
+   			//endmsgque
 			waitpid(pid, 0, 0);
-
 			printf("AFTERSERVER\n");
+			
 			read(client_to_server, buf, BUFSIZ);
-
-			if (strcmp("", buf) != 0)
-      			{
+			
+			if (strcmp("", buf) != 0) {
          			printf("Received: %s\n", buf);
-         			//write(server_to_client,buf,BUFSIZ);
-      			}
-
+         			//write(server_to_client, strI, sizeof(strI));
+      			}			 
+			
 			close(client_to_server);
    			close(server_to_client);
+   	
 		}
 		
 		pid = fork();
@@ -74,7 +114,26 @@ int main(int argc, char* argv[]) {
 		
 			client_to_server = open(myfifo1, O_RDONLY);
    			server_to_client = open(myfifo2, O_WRONLY);
+			
+			//msgque
+   			printf("Enter lines of text, ^D to quit:\n");
+			Buf.mtype = 1;
+	
+			/*while(*/fgets(Buf.mtext, sizeof Buf.mtext, stdin); //!= NULL) {
+        		int len = strlen(Buf.mtext);
 
+        		if (Buf.mtext[len-1] == '\n') Buf.mtext[len-1] = '\0';
+
+        		if (msgsnd(msqid, &Buf, len+1, 0) == -1) /* +1 for '\0' */
+            			perror("msgsnd");
+    			//}
+
+   			if (msgctl(msqid, IPC_RMID, NULL) == -1) {
+        			perror("msgctl");
+        			exit(1);
+    			}
+   			//endmsgque
+			
 			waitpid(pid, 0, 0);
 			
 			printf("AFTERSERVER\n");
@@ -85,7 +144,9 @@ int main(int argc, char* argv[]) {
          			printf("Received: %s\n", buf);
          			//write(server_to_client,buf,BUFSIZ);
       			}	
-      	
+      			
+      			//memset(buf, 0, sizeof(buf));
+      			
 			close(client_to_server);
    			close(server_to_client);
 		}
--- new.c
+++ new.c
@@ -5,8 +5,29 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct my_msgbuf {
+    long mtype;
+    char mtext[200];
+};
 
 int main(int argc, int *argv[]) {
+	struct my_msgbuf Buf;
+    	int msqid;
+    	key_t key;
+    	
+    	if ((key = ftok("fork_exec.c", 'B')) == -1) {
+        	perror("ftok");
+        	exit(1);
+    	}
+
+    	if ((msqid = msgget(key, 0644)) == -1) {
+        	perror("msgget");
+        	exit(1);
+    	}
+
 	int client_to_server;
    	char *myfifo1 = "/tmp/client_to_server_fifo";
 
@@ -14,18 +35,34 @@ int main(int argc, int *argv[]) {
    	char *myfifo2 = "/tmp/server_to_client_fifo";
 
    	char str[BUFSIZ];
+
    	printf("Input message to server: ");
    	scanf("%s", str);
 
 	client_to_server = open(myfifo1, O_WRONLY);
    	server_to_client = open(myfifo2, O_RDONLY);
+
    	write(client_to_server, str, sizeof(str));
 
+	printf("CLIENT: ready to receive messages, captain.\n");
+
+    	//for(;;) {
+        	if (msgrcv(msqid, &Buf, sizeof Buf.mtext, 0, 0) == -1) {
+            		perror("msgrcv");
+            		exit(1);
+        	}
+        	printf("CLIENT: \"%s\"\n", Buf.mtext);
+    	//}
+	
 	int i = 0;
 	while(i < argc) {
 	printf("arg[%d]: %s\n", i, argv[i]);
 		i++;
 	}
+	
+	//read(server_to_client, buf, BUFSIZ);
+
+	//printf("...received from the server: %s\n",str);
 
 	close(client_to_server);
     	close(server_to_client);
